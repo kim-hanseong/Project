a35ecd58bdc0693cf0a9bcdd1464034df4cc3f65
hanseong Kim <gkstjd20001@gmail.com>
2025-05-26 21:11:32 +0900

    Initial commit

--- components/bestseller/bestseller-Category-Select/Desktop/index.tsx
+++ components/bestseller/bestseller-Category-Select/Desktop/index.tsx
@@ -1,4 +1,5 @@
 import { IoIosArrowUp } from "react-icons/io";
+import { IconBaseProps } from "react-icons";
 
 import styles from "@/components/bestseller/bestseller-Category-Select/Desktop/index.module.css";
 import PopButton from "@/components/전역/PopButton";
@@ -11,6 +12,10 @@ interface BestSellerCategoryProps {
   categoryList: CategoryItem[]; // CATEGORY_LIST를 인수로 받을 타입
   handleCategoryChange: (category: string) => void; // 카테고리 변경 핸들러 타입
 }
+
+const IconWrapper = (props: IconBaseProps) => {
+  return <IoIosArrowUp {...props} />;
+};
 
 const BestSellerDesktopCategor: React.FC<BestSellerCategoryProps> = ({
   props,
@@ -41,7 +46,7 @@ const BestSellerDesktopCategor: React.FC<BestSellerCategoryProps> = ({
       ButtonTrigger={<>{props}</>}
       ButtonIcons={
         <span>
-          <IoIosArrowUp />
+          <IconWrapper />
         </span>
       }
       ButtonContent={<RenderCategory />}
